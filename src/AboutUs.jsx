2d0d44d1a224f9f16c0d352d073f02c6a4dcd09d
shivwagh3055 <shivwagh3055@gmail.com>
2025-10-25 19:51:42 +0530

    Update company name from Shreyash's to Shiv's

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -8,9 +8,9 @@ function AboutUs() {
   return (
     <div className="about-us-container">
       {/* <h1 className="about-us-heading">About Us</h1> */}
-      <p className="about-us-description">Welcome to Shreyash's Plants, where green meets serenity!</p>
+      <p className="about-us-description">Welcome to Shiv's Plants, where green meets serenity!</p>
       <p className="about-us-content">
-      At Shreyash's Plants, we offer a wide variety of lush, healthy plants, from resilient succulents and delicate orchids to robust trees and vibrant flowering plants, each carefully selected to thrive in your specific environment. Our user-friendly website provides detailed care instructions, gardening tips, and expert advice to ensure your plants flourish. Whether you're an experienced gardener or just starting your green journey, Shreyash's Plants is your trusted partner in cultivating a verdant and thriving oasis. Join our community of plant enthusiasts and let Shreyash's Plants help you grow a greener world.
+      At Shiv's Plants, we offer a wide variety of lush, healthy plants, from resilient succulents and delicate orchids to robust trees and vibrant flowering plants, each carefully selected to thrive in your specific environment. Our user-friendly website provides detailed care instructions, gardening tips, and expert advice to ensure your plants flourish. Whether you're an experienced gardener or just starting your green journey, Shreyash's Plants is your trusted partner in cultivating a verdant and thriving oasis. Join our community of plant enthusiasts and let Shreyash's Plants help you grow a greener world.
       </p>
       {/* <p className="plant_logo_left"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
       <p className="about-us-content">
